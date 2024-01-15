97f30bbcb58a619ce249798b9a30bc56126c01c8
Ethan Woo <ethan.cpw.woo@outlook.com>
2024-01-15 18:11:29 -0500

    converted csv to txt

---
+++ utils/csv_to_text.py
@@ -0,0 +1,22 @@
+import csv
+
+
+def main():
+
+    with open('../src/wiki_movie_plots_deduped.csv', encoding='utf8', newline='') as data: 
+        with open('../src/summaries.txt', 'w', encoding='utf8') as file:
+            reader = csv.reader(data)
+            for row in reader:
+                print('writing... \n')
+                file.write(f'Title: {row[1]} \n')
+                file.write(f'Genre: {row[5]} \n')
+                file.write(f'Description: \n')
+                for word in row[7]:
+                    if word != '\n':
+                        file.write(word)
+                file.write('\n' + '\n')
+
+    print('done')
+
+if __name__ == '__main__':
+    main()
