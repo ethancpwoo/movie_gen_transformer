66407b067ac9306e459b1f0dd1a96c6547a5af6e
Ethan Woo <ethan.cpw.woo@outlook.com>
2024-01-23 22:52:02 -0500

    added attention to new model and working well

--- src/movie_gen.py
+++ src/movie_gen.py
@@ -13,10 +13,10 @@ from torch.nn import functional as F
 batch_size = 64 # how many sequences will be processed in parallel
 block_size = 256 # how many tokens/nodes are we taking into context
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_interval = 200
+eval_interval = 500
 eval_iters = 200
 head_size = 16 #size of attention head
-learning_rate = 3e-4 # small network so really aggresive learning rate
+learning_rate = 1e-3 # small network so really aggresive learning rate
 max_iters = 5000
 n_embd = 384
 n_heads = 6
@@ -48,7 +48,6 @@ def encode(s : str, enc_map : dict) -> list:
 
 def decode(ls : list, dec_map : dict) -> str:
     char_list = []
-    ls = ls.tolist()
     for i in ls: 
         char_list.append(dec_map[i]) 
     s = ''.join(char_list)
@@ -85,27 +84,94 @@ def get_batch(split : str, train_data : list, test_data : list):
     return x, y
 
 @torch.no_grad()
-def estimate_loss():
+def estimate_loss(train_data : list, test_data : list):
     out = {}
     model.eval()
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
-            X, Y = get_batch(split)
+            X, Y = get_batch(split, train_data, test_data)
             logits, loss = model(X, Y)
             losses[k] = loss.item()
         out[split] = losses.mean()
     model.train()
     return out
 
+class Head(nn.Module):
+
+    # Single head of self-attention
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False) # size = (B, T, C).
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        # triangle matrix (block size, block size)
+    
+    def forward(self, x):
+
+        # Instead of summing the values in the tensor, now will have a query and a key.
+        # Query is what I am looking for, Key is what I contain in terms of weight.
+        # Ex: if I am vowel, my key will be align well with query of constanants will have a high affinity.
+
+        B, T, C = x.shape
+
+        # Single Head of self-attention (normally chat-gpt will have mutliple heads for increased accuracy for attention)
+        # linear transformation template of y = x(A (transpose) ) + b
+        # independently generated keys and query so they do not have any affinity yet. 
+
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+
+        #here is the dot product to see which of the values generate affinity
+        # Affinity between tokens in tensor, dot of key and query = wei.
+        wei = q @ k.transpose(-2, -1) * k.shape[-1]**-0.5# due to batch dimension. (B, T, 16) @ (B, 16, T) => (B, T, T)
+        
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) #makes this a decoder attention block
+
+        #softmax is normalization function which defines summing and meaning.
+        wei = F.softmax(wei, dim=-1)
+
+        # v are the elements we aggregate, not raw x. X is sort of private to this token.
+        out = wei @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    # Stacked attention modules
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        # self.proj = nn.Linear(head_size * num_heads, n_embd)
+    
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        return out
+
 class MovieModel(nn.Module):
 
-    def __init__(self, vocab_size):
-        super.__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.self_attention_heads = MultiHeadAttention(4, n_embd//4)
+        self.language_modeling_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
-        logits = self.token_embedding_table(idx)
+        """
+        Definitions:
+        logits: outputs of nn before activation function
+        tokens: inputs for nn
+        """
+        B, T = idx.shape
+        token_embeddings = self.token_embedding_table(idx)
+        position_embeddings = self.position_embedding_table(torch.arange(T, device=device))
+
+        x = token_embeddings + position_embeddings
+        x = self.self_attention_heads(x)
+
+        logits = self.language_modeling_head(x)
 
         if targets is None:
             loss = None
@@ -114,22 +180,30 @@ class MovieModel(nn.Module):
             logits = logits.view(B*T, C)
             targets = targets.view(B*T)
             loss = F.cross_entropy(logits, targets)
+
         return logits, loss
     
-    def generate(self, idx, max_new_tokens): 
+    def generate(self, idx, max_new_tokens):
+
         for _ in range(max_new_tokens):
+            # crop idx to block size tokens
             idx_cond = idx[:, -block_size:]
+            # get the predictions
             logits, loss = self(idx_cond)
+            # focus on last step only
             logits = logits[:, -1, :]
+            # softmax
             probs = F.softmax(logits, dim=-1)
+            # sample from distrobution
             idx_next = torch.multinomial(probs, num_samples=1)
+            # append the focused idx to the previous sequence
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
 
 print(device)
 train_data, test_data = set_data(text, enc_map)
 
-model = MovieModel(vocab_size)
+model = MovieModel()
 model = model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
@@ -137,7 +211,7 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 for iter in range(max_iters):
 
     if iter % eval_interval == 0:
-        losses= estimate_loss()
+        losses= estimate_loss(train_data, test_data)
         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
     
     xb, yb = get_batch('train', train_data, test_data)
